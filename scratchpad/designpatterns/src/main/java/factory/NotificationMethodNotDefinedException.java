aa3772264e4239d37116e908a793941fe8ace457
aschatt <aschatt@localhost>
2007-12-19 00:30:08 +0000

    Factory Pattern and tests added

---
+++ scratchpad/designpatterns/src/main/java/factory/NotificationMethodNotDefinedException.java
@@ -0,0 +1,15 @@
+package factory;
+
+public class NotificationMethodNotDefinedException extends Exception {
+
+	private static final long serialVersionUID = -2567118931275910282L;
+
+	public NotificationMethodNotDefinedException () {
+		super();
+	}
+	
+	public NotificationMethodNotDefinedException (String s) {
+		super(s);
+	}
+
+}
