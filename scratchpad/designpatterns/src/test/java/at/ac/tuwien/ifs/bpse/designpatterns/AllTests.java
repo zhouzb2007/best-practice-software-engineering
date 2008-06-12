622bfd6851e55c61b3f1f1b537f12d456352974b
aschatt <aschatt@localhost>
2008-06-12 21:22:02 +0000

    Observer Pattern, Beispiel und Java Code

--- scratchpad/designpatterns/src/test/java/at/ac/tuwien/ifs/bpse/designpatterns/AllTests.java
+++ scratchpad/designpatterns/src/test/java/at/ac/tuwien/ifs/bpse/designpatterns/AllTests.java
@@ -2,6 +2,8 @@ package at.ac.tuwien.ifs.bpse.designpatterns;
 
 import at.ac.tuwien.ifs.bpse.designpatterns.delegation.NotificationManagerTest;
 import at.ac.tuwien.ifs.bpse.designpatterns.factory.NotificationFactoryTest;
+import at.ac.tuwien.ifs.bpse.designpatterns.observer.DamTest;
+import at.ac.tuwien.ifs.bpse.designpatterns.observer.ObserverTest;
 import at.ac.tuwien.ifs.bpse.designpatterns.singleton.NoficationManagerServiceTest;
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -15,6 +17,8 @@ public class AllTests {
 		suite.addTestSuite(NotificationManagerTest.class);
 		suite.addTestSuite(NotificationFactoryTest.class);
 		suite.addTestSuite(NoficationManagerServiceTest.class);
+		suite.addTestSuite(DamTest.class);
+		suite.addTestSuite(ObserverTest.class);
 		//$JUnit-END$
 		return suite;
 	}
