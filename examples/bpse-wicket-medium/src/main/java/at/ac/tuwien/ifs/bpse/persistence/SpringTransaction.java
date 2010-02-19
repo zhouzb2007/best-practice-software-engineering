594e8e185c15513f80dfb9a99c07514c93983ea9
Erik Gostischa-Franta <gostischa-franta@qse.ifs.tuwien.ac.at>
2010-02-19 01:07:20 +0100

    added new medium sample. iBatis 3 + Spring + Apache Wicket

---
+++ examples/bpse-wicket-medium/src/main/java/at/ac/tuwien/ifs/bpse/persistence/SpringTransaction.java
@@ -0,0 +1,46 @@
+package at.ac.tuwien.ifs.bpse.persistence;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.transaction.Transaction;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+
+/**
+ * iBATIS 3 transaction that is managed by Spring.
+ * This class sourced from:
+ * http://opensource.atlassian.com/confluence/oss/display/IBATIS/Spring+integration+with+iBATIS+3
+ *
+ * @author poitrac
+ */
+public class SpringTransaction implements Transaction {
+
+    private final DataSource dataSource;
+    private final Connection connection;
+
+    SpringTransaction(DataSource dataSource, Connection connection) {
+        this.connection = connection;
+        this.dataSource = dataSource;
+    }
+
+    public void close() throws SQLException {
+        if (dataSource instanceof TransactionAwareDataSourceProxy) {
+            connection.close();
+        } else {
+            DataSourceUtils.doReleaseConnection(connection, dataSource);
+        }
+    }
+    public void commit() throws SQLException {
+        // Commit is done elsewhere.
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+    public void rollback() throws SQLException {
+        // Rollback is done elsewhere.
+    }
+
+}
