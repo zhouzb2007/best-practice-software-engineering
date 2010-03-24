f7da36e4ebd83522e45ca00442b489302eb50593
Markus Demolsky <mdemolsky@gmail.com>
2010-03-24 20:03:34 +0100

    Enhance Student service with Use Cases for the Web Application Add Default implementation for the Student Service Add Course Service Interface and Default implementation for the course service

---
+++ examples/core/src/main/java/at/ac/tuwien/ifs/bpse/services/CourseService.java
@@ -0,0 +1,78 @@
+package at.ac.tuwien.ifs.bpse.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import at.ac.tuwien.ifs.bpse.domain.Course;
+import at.ac.tuwien.ifs.bpse.domain.Student;
+
+/**
+ * Default implementation of the Course Service
+ * @author SE Team
+ *
+ */
+public class CourseService implements ICourseService {
+	private Logger log = Logger.getLogger(CourseService.class);
+
+	
+	@Override
+	public List<Course> getCoursesByName(String name) {
+		log.info("Search for courses by name: " + name);
+		Course c1 = new Course();
+		c1.setId(1);
+		c1.setTitle("Course 1");
+		Course c2 = new Course();
+		c2.setId(1);
+		c2.setTitle("Course 2");
+		
+		List<Course> courses = new ArrayList<Course>();
+		courses.add(c1);
+		courses.add(c2);
+		
+		return courses;
+	}
+
+	@Override
+	public boolean registerToCourse(Student student, Course course) {
+		log.info("Register Student " + student.getFullname() + " for course: " + course.getTitle());
+		return false;
+	}
+
+	@Override
+	public boolean unregisterFromCourse(Student student, Course course) {
+		log.info("Unregister Student " + student.getFullname() + " for course: " + course.getTitle());
+		return false;
+	}
+
+	@Override
+	public List<Course> getCoursesForStudent(String name, Student student) {
+		log.info("Search for courses by name: " + name + " only for student " + student.getFullname());
+		Course c1 = new Course();
+		c1.setId(1);
+		c1.setTitle("Course 1");
+		Course c2 = new Course();
+		c2.setId(1);
+		c2.setTitle("Course 2");
+		
+		List<Course> courses = new ArrayList<Course>();
+		courses.add(c1);
+		courses.add(c2);
+		
+		return courses;
+	}
+
+	@Override
+	public Course saveCourse(Course course) {
+		log.info("Save courses " + course.getTitle() + " ," + course.getEcts());
+		return course;
+	}
+
+	@Override
+	public boolean removeCourse(Course course) {
+		log.info("Remove courses " + course.getTitle() + " ," + course.getEcts());
+		return false;
+	}
+
+}
