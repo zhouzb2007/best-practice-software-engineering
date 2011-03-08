33422325d363ca25e72c688e1a034bc2946bd951
Erik Gostischa-Franta <gostischa-franta@qse.ifs.tuwien.ac.at>
2011-03-08 18:49:03 +0100

    removing old basic archetype entirely, because maven3 now supports much better archetype generation from existing Projects

--- examples/basic-archetype/src/main/resources/archetype-resources/src/main/java/dao/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * The DAO Package contains all the <i>Data Access Objects</i>.
- * DAOs provide access to the persistence mechanism.
- * @author The SE-Team
- * @version 1.0
- * @see <a href="http://www.springframework.org/documentation"
- *   target="newWindow">Springframework-website</a>
- */
-package ${groupId}.dao;
--- examples/basic-archetype/src/main/resources/archetype-resources/src/test/java/test/export/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Unit Tests for Export/Import.
- * 
- * @author The SE-Team
- * @version 1.0
- */
-package ${groupId}.test.export;
